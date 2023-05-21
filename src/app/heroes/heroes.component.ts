77325cb19c0dbf764f229adc4c76df58d15d8698
tom.lee1 <tom.lee@thortful.com>
2023-05-21 17:35:10 +0100

    adds current progress

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Bob Fantastic'},
+  { id: 13, name: 'Magneta'},
+  { id: 14, name: 'RubberMan'},
+  { id: 15, name: 'Catman'},
+  { id: 16, name: 'Magma'},
+  { id: 17, name: 'Tornado Tony'},
+  { id: 18, name: 'Mr. Nice'},
+]
